73f91de4ac5edeebca3e044df10cc2f8ba4a7944
ridafd <ridafakherlden@gmail.com>
2020-10-08 15:14:45 +0300

    Episode 11:Practical Components Exercise #3: Tabs

--- main.js
+++ main.js
@@ -1,26 +1,82 @@
-Vue.component('modal', {
+Vue.component('tabs', {
+	template: `
+		<div>
+			<div class="tabs">
+			  <ul>
+
+			    <li v-for='tab in tabs' :class="{ 'is-active': tab.isActive }">
+	   			 	<a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+			    </li>
+
+			  </ul>
+			</div>
+
+			<div class="tabs-details">
+				<slot></slot>
+			</div>
+		</div>
+	`,
+
+	data() {
+
+		return { tabs: [] };
+	},
+
+	created() {
+		this.tabs = this.$children;
+	},
+
+	methods: {
+
+		selectTab(selectedTab) {
+
+			this.tabs.forEach(tab => {
+
+				tab.isActive = (tab.name == selectedTab.name);
+			})
+		}
+	}
+});
+
+
+
+Vue.component('tab', {
 
 	template: `
-		<div class="modal is-active">
-		  	<div class="modal-background"></div>
-	  		<div class="modal-content">
+		<div v-show="isActive"><slot></slot></div>
+	`,
 
-	  			<div class="box">
-	  				<slot></slot>
-	  			</div>
+	props: {
 
-			</div>		
-		  <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
-		</div>
-	`
+		name: { required: true },
+		selected: { default: false }
+	},
+
+	data() {
+
+		return {
+
+			isActive: false			
+		};
+	},
+
+
+	computed: {
+
+		href() {
+
+			//about-our-culture
+			return '#' + this.name.toLowerCase().replace(/ /g, '-');
+		}
+	},
+
+	mounted() {
+
+		this.isActive = this.selected;
+	}
 });
 
 new Vue({
 
-	el: '#root',
-
-	data: {
-
-		showModal: false
-	}
+	el: '#root'
 });
