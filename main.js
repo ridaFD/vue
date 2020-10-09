9cd94fb32c4a449d11dc5d5efd3c336a3710e721
ridafd <ridafakherlden@gmail.com>
2020-10-09 15:28:01 +0300

    Episode 15:Single-Use Components and Inline Templates

--- main.js
+++ main.js
@@ -1,36 +1,11 @@
-Vue.component('modal', {
+Vue.component('progress-view', {
 
-	template: `
+	data() {
 
-			<div class="modal is-active">
-			  <div class="modal-background"></div>
+		return { completionRate: 50 };
+	}
 
-			  <div class="modal-card">
-
-			    <header class="modal-card-head">
-			      <p class="modal-card-title">
-
-			      	<slot name="header"></slot>
-
-			      </p>
-			      <button class="delete" aria-label="close"></button>
-			    </header>
-
-			    <section class="modal-card-body">
-
-					<slot></slot>			      
-
-			    </section>
-
-			    <footer class="modal-card-foot">
-
-			      <slot name="footer"></slot>
-			      
-			    </footer>
-
-			  </div>		
-			</div>
-	`
+	
 })
 
 
