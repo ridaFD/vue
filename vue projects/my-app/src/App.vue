ef52416ad5220fdc361447bc4d43a5201366416d
ridafd <ridafakherlden@gmail.com>
2020-10-10 12:03:20 +0300

    Episode 16:Webpack and vue-cli

---
+++ vue projects/my-app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Message>Hello there</Message>
+  </div>
+</template>
+
+<script>
+import Message from "./components/Message.vue";
+export default {
+  name: 'app',
+  components: { Message },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+</style>
