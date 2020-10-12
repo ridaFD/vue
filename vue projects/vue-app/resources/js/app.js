c01ec53e650f71733f1d0b7ba5929135f4d544a3
ridafd <ridafakherlden@gmail.com>
2020-10-12 10:58:33 +0300

    Episode 18:Vue Ajax Requests With Axios

---
+++ vue projects/vue-app/resources/js/app.js
@@ -0,0 +1,11 @@
+// window.Vue = require('vue');
+Vue.prototype.$http = axios;
+new Vue({
+    el: '#app',
+    data: {
+        skills:  []
+    },
+    mounted() {
+        this.$http.get('/skills').then(response => this.skills = response.data);
+    }
+});
---
+++ vue projects/vue-app/resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+    </head>
+    <body>
+        <div id="app">
+
+            <ul>
+                <li v-for="skill in skills" v-text="skill"></li>
+            </ul>
+    
+        </div>
+
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+        <script src="https://unpkg.com/vue"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
