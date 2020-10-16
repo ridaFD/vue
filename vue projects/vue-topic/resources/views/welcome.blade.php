add5f22f9a18c58edb5598afc0c5a2fa5632c31b
ridafd <ridafakherlden@gmail.com>
2020-10-16 13:05:05 +0300

    Episode 25:Custom Input Components

--- vue projects/vue-topic/resources/views/welcome.blade.php
+++ vue projects/vue-topic/resources/views/welcome.blade.php
@@ -1,22 +1,22 @@
-{{--<!DOCTYPE html>--}}
-{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
-{{--    <head>--}}
-{{--        <meta charset="utf-8">--}}
-{{--        <meta name="viewport" content="width=device-width, initial-scale=1">--}}
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-{{--        <title>Laravel</title>--}}
+        <title>Laravel</title>
 
-{{--    </head>--}}
-{{--    <body>--}}
-{{--        <div id="app">--}}
-{{--            <input type="text" v-model="title">--}}
-{{--            <input type="text" :value="coupon" @input="coupon = $event.target.value">--}}
+    </head>
+    <body>
+        <div id="app">
+            <input type="text" v-model="title">
+            <input type="text" :value="coupon" @input="coupon = $event.target.value">
 
-{{--            <coupon v-model="coupon"></coupon>--}}
-{{--        </div>--}}
+            <coupon v-model="coupon"></coupon>
+        </div>
 
-{{--        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>--}}
-{{--        <script src="https://unpkg.com/vue@2.6.12/dist/vue.js"></script>--}}
-{{--        <script src="{{ asset('js/app.js') }}"></script>--}}
-{{--    </body>--}}
-{{--</html>--}}
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+        <script src="https://unpkg.com/vue@2.6.12/dist/vue.js"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
